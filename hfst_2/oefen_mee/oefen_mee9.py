e24cf53c6d66aafba2f4f6ae37a0f10e8f88c19b
Ziyaad <Ziyaadwa1@gmail.com>
2022-10-19 09:00:40 +0200

    update

--- hfst_2/oefen_mee/oefen_mee9.py
+++ hfst_2/oefen_mee/oefen_mee9.py
@@ -1,7 +1,18 @@
 import json
 
-fp = open("hfst_2/oefen_mee/oefen_mee8.json", "r")
+fp = open("hfst_2/oefen_mee/oefen_mee9.json", "r")
 quiz = json.load(fp)
 fp.close()
 
 print(quiz)
+
+for key,onderwerp in quiz["quiz"].items():
+    for key2, vraag in onderwerp.items():
+        print(vraag["vraag"])
+        print(vraag["opties"])
+        
+        keuze = input("Kies een antwoord: ")
+        if keuze == vraag["antwoord"]:
+            print("Het antwoord is correct!")
+        else: 
+            print("Het antwoord is niet correct.")   

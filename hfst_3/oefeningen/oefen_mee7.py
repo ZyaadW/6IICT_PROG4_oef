dd151accf2f3a92ff5ef5978550491978b949861
Ziyaad <Ziyaadwa1@gmail.com>
2022-11-30 09:24:59 +0100

    grtg

--- hfst_3/oefeningen/oefen_mee7.py
+++ hfst_3/oefeningen/oefen_mee7.py
@@ -1,9 +1,25 @@
 """ Niveau 1 """
 num_lijst = [ 100, 101, 0, "103", 104 ]
-index = int( input( "Geef een index: " ) )
-
-""" Niveau 2 (haal uit commentaar) """
-# print( f"100 / {num_lijst[index]} = {100/num_lijst[index]}" ) 
-
-""" Niveau 3 (haal uit commentaar) """
-# print( "Geldig getal als index opgegeven." )
+try:
+    index = int( input( "Geef een index: " ) )
+except TypeError:
+    print("ingegeven waarde is geen geheel getal")
+except IndexError:
+    print("index ligt buiten bereik")
+except ValueError:
+    print("De ingegeven waarde is geen getal")
+else:
+    try:
+        # """ Niveau 2 (haal uit commentaar) """
+        print( f"100 / {num_lijst[index]} = {100/num_lijst[index]}" ) 
+    except TypeError:
+        print("ingegeven waarde is geen geheel getal")
+    except IndexError:
+        print("index ligt buiten bereik")
+    except ValueError:
+        print("De ingegeven waarde is geen getal") 
+    except ZeroDivisionError:
+        print("Het getal 0 kan niet worden opegegeven")
+# """ Niveau 3 (haal uit commentaar) """
+finally:
+    print( "Het programma is beëndigd")

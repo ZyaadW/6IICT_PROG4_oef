fdf21c299e65e7838eeaf987676b33066696d094
HendrikClijstersMosa <hendrik.clijsters@mosa-rt.be>
2023-05-31 08:13:46 +0200

    Opdracht 2 gewijzigd

--- hfst_8_recursie/opdrachten/opdracht2.py
+++ hfst_8_recursie/opdrachten/opdracht2.py
@@ -1,6 +1,9 @@
-""" Schrijf een recursieve functie die controleert of een lijst geordend is. """
+"""
+Bepaal recursief de som van alle elementen in een lijst.
+De lijst bestaat uit veel verschillende niveau's.
+"""
 
-print( geordend([1, 2, 3, 4]) ) # True
-print( geordend([5, 2, 8, 4]) ) # False
-print( geordend([9]) )          # True
-print( geordend([0,1,2,4,3,5])) # False
+print( som_lijst([1,2,3,4]) )           # 10
+print( som_lijst([1, [2,3], 4]) )       # 10
+print( som_lijst([1,2,[3,[4]]]) )       # 10
+print( som_lijst([1, [2, [3, [4]]]]) )  # 10
